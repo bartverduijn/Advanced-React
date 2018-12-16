154fef06f4ef8941b2598a0706d3e481cd0229d7
bartverduijn <23431875+bartverduijn@users.noreply.github.com>
2018-12-16 15:04:30 +0100

    bake styles 💅

---
+++ sick-fits/frontend/pages/_document.js
@@ -0,0 +1,23 @@
+/*
+💅 To prerender Styled Components, copied from https://github.com/zeit/next.js/blob/canary/examples/with-styled-components/pages/_document.js
+*/
+
+import Document from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+class MyDocument extends Document {
+    static async getInitialProps(ctx) {
+        const sheet = new ServerStyleSheet();
+
+        const originalRenderPage = ctx.renderPage;
+        ctx.renderPage = () =>
+            originalRenderPage({
+                enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+            });
+
+        const initialProps = await Document.getInitialProps(ctx);
+        return { ...initialProps, styles: [...initialProps.styles, ...sheet.getStyleElement()] };
+    }
+}
+
+export default MyDocument;
