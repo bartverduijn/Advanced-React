8073605121174f1b556a60d2b4c71e5a5c9d41c3
bartverduijn <23431875+bartverduijn@users.noreply.github.com>
2018-12-16 20:44:06 +0100

    initial setup 🧘

---
+++ sick-fits/backend/src/db.js
@@ -0,0 +1,11 @@
+// Connects to Prisma DB and let me query the db with js
+const { Prisma } = require('prisma-binding');
+
+const db = new Prisma({
+    typeDefs: 'src/generated/prisma.graphql',
+    endpoint: process.env.PRISMA_ENDPOINT,
+    secret: process.env.PRISMA_SECRET,
+    debug: false,
+});
+
+module.exports = db;
--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,21 @@
-// let's go!
+require('dotenv').config({ path: 'variables.env' });
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+// TODO: Use middleware to handle JWT cookies
+
+// TODO: Use middleware to populate current user
+
+server.start(
+    {
+        cors: {
+            credentials: true,
+            origin: process.env.FRONTEND_URL,
+        },
+    },
+    data => {
+        console.log(`💻  Server is now running on port http://localhost:${data.port}`);
+    }
+);
