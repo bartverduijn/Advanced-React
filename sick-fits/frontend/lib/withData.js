717ffc8005022324bf17595aea4021fbf98594cd
bartverduijn <23431875+bartverduijn@users.noreply.github.com>
2018-12-30 19:27:53 +0100

    Setting up apollo  🚀

--- sick-fits/frontend/lib/withData.js
+++ sick-fits/frontend/lib/withData.js
@@ -1,15 +1,14 @@
-import withApollo from 'next-with-apollo';
-import ApolloClient from 'apollo-boost';
-import { endpoint } from '../config';
+import withApollo from 'next-with-apollo'; // Expose Apollo as a prop in Next.js
+import ApolloClient from 'apollo-boost'; // Recommended preset of Apollo packages
+import { endpoint } from '../config'; // Location of our API
 
 function createClient({ headers }) {
     return new ApolloClient({
         uri: process.env.NODE_ENV === 'development' ? endpoint : endpoint,
         request: operation => {
             operation.setContext({
-                fetchOptions: {
-                    credentials: 'include',
-                },
+                // Bring credential cookies with every request
+                fetchOptions: { credentials: 'include' },
                 headers,
             });
         },
--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,18 +1,35 @@
 import App, { Container } from 'next/app';
+import { ApolloProvider } from 'react-apollo';
 import Page from '../components/Page';
+import withData from '../lib/withData';
 
 class MyApp extends App {
+    // To handle page numbers in URI (eg /items?page=1) => Crawl all pages and fetch/return all data
+    static async getInitialProps({ Component, ctx }) {
+        let pageProps = {};
+        // If Component I'm trying to render has some props
+        if (Component.getInitialProps) {
+            // Serve them via the pageProps
+            pageProps = await Component.getInitialProps(ctx);
+        }
+        // To expose query to the user
+        pageProps.query = ctx.query;
+        return pageProps;
+    }
+
     render() {
-        const { Component } = this.props;
+        const { Component, apollo, pageProps } = this.props;
 
         return (
             <Container>
-                <Page>
-                    <Component />
-                </Page>
+                <ApolloProvider client={apollo}>
+                    <Page>
+                        <Component {...pageProps} />
+                    </Page>
+                </ApolloProvider>
             </Container>
         );
     }
 }
 
-export default MyApp;
+export default withData(MyApp); // Wrapped in higher-order component to make this.props.apollo available
