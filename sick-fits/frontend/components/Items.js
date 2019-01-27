f1a302d18cfb61aa691d1affeaa02e220191f5c7
bartverduijn <23431875+bartverduijn@users.noreply.github.com>
2019-01-27 18:44:26 +0100

    Added pagination 📖

--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -1,12 +1,14 @@
+import PropTypes from 'prop-types';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import styled from 'styled-components';
 import Item from './Item';
 import Pagination from './Pagination';
+import { perPage } from '../config';
 
 const ALL_ITEMS_QUERY = gql`
-    query ALL_ITEMS_QUERY {
-        items {
+    query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
+        items(first: $first, skip: $skip, orderBy: createdAt_DESC) {
             id
             title
             price
@@ -32,7 +34,14 @@ const ItemsList = styled.div`
 const Items = ({ page }) => (
     <Center>
         <Pagination page={page} />
-        <Query query={ALL_ITEMS_QUERY}>
+        <Query
+            query={ALL_ITEMS_QUERY}
+            variables={{
+                skip: page * perPage - perPage,
+            }}
+            // 👇 Don't use caching for pagination, the pages refresh if you added or deleted an item.
+            // fetchPolicy="network-only"
+        >
             {({ data, error, loading }) => {
                 if (loading) return <p>Loading...</p>;
                 if (error) return <p>Error: {error.message}</p>;
@@ -49,6 +58,10 @@ const Items = ({ page }) => (
         <Pagination page={page} />
     </Center>
 );
+
+Items.propTypes = {
+    page: PropTypes.number.isRequired,
+};
 
 export default Items;
 export { ALL_ITEMS_QUERY };
--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,5 +1,12 @@
+import PropTypes from 'prop-types';
 import Items from '../components/Items';
 
-const Home = ({ query }) => <Items page={parseFloat(query.page) || 1} />; // If no ?page= in url, return 1 instead of NaN
+const Home = ({ query: { page } }) => <Items page={parseFloat(page) || 1} />; // If no ?page= in url, return 1 instead of NaN
+
+Home.propTypes = {
+    query: PropTypes.shape({
+        page: PropTypes.string,
+    }),
+};
 
 export default Home;
