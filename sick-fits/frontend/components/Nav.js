809368ceb399bdef5f43a96b6c91b7a2117f6faf
bartverduijn <23431875+bartverduijn@users.noreply.github.com>
2021-03-28 17:11:52 +0200

    Update formatting

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,4 +1,4 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
 export default function Nav() {
 	return (
@@ -8,5 +8,5 @@ export default function Nav() {
 			<Link href="/orders">Orders</Link>
 			<Link href="/account">Account</Link>
 		</nav>
-	)
+	);
 }
